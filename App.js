9c70f12be55b3aa93d0f9a6cf6b9e9644f7760e8
Paulo09 <paullocasttro@hotmail.com>
2024-01-17 21:28:38 -0300

    Funcionalidade

--- App.js
+++ App.js
@@ -2,9 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import PublicRoutes from './src/routes/PublicRoutes';
 import PrivateRoutes from './src/routes/PrivateRoutes';
-
 import AuthContext from './src/contexts/AuthContext';
 import { useState } from 'react';
+import 'react-native-gesture-handler';
 
 export default function App() {
 
--- src/routes/PrivateRoutes.js
+++ src/routes/PrivateRoutes.js
@@ -1,8 +1,11 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../screens/HomeScreen";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
-const Stack = createNativeStackNavigator();
+//const Stack = createNativeStackNavigator();
+
+const Stack = createDrawerNavigator();
 
 export default function PrivateRoutes() {
     return(
